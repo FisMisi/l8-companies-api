143ddee0cf1a9f78171833c920da130d78600868
Mihály Markó <mihaly.richard.marko@gmail.com>
2022-12-12 14:18:44 +0100

    import companies from csv+

--- app/Console/Commands/ImportCompanies.php
+++ app/Console/Commands/ImportCompanies.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands;
 
 use App\Imports\CompaniesImport;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Storage;
 
 class ImportCompanies extends Command
 {
